a8965a4e1777e2f97c20830a130cafb04a5ed3c1
Luan Grabher <lujetix@hotmail.com>
2020-05-15 11:48:04 -0300

    Added JExcelStyles to help styles

---
+++ src/JExcel/JExcelStyles.java
@@ -0,0 +1,32 @@
+package JExcel;
+
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+
+public class JExcelStyles {
+
+    private void setCellBorders(CellStyle style) {
+        style.setBorderTop(BorderStyle.THIN);
+        style.setBorderLeft(BorderStyle.THIN);
+        style.setBorderRight(BorderStyle.THIN);
+        style.setBorderBottom(BorderStyle.THIN);
+    }
+
+    private void removeCellBorders(CellStyle style) {
+        style.setBorderTop(BorderStyle.NONE);
+        style.setBorderLeft(BorderStyle.NONE);
+        style.setBorderRight(BorderStyle.NONE);
+        style.setBorderBottom(BorderStyle.NONE);
+    }
+
+    private void setStylesInRange(XSSFSheet sheet, CellStyle estilo,
+            int primeira_linha, int ultima_linha,
+            int primeira_coluna, int ultima_coluna) {
+        for (int linha = primeira_linha; linha <= ultima_linha; linha++) {
+            for (int coluna = primeira_coluna; coluna <= ultima_coluna; coluna++) {
+                sheet.getRow(linha).getCell(coluna).setCellStyle(estilo);
+            }
+        }
+    }
+}
