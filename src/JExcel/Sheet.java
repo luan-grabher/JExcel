3205ec2c5b271a681fce66d9d94351fe475f356f
TI01 <TI01@TI01.MORESCO.LOCAL>
2021-09-01 16:50:53 -0300

    Fix range matches

--- src/JExcel/Sheet.java
+++ src/JExcel/Sheet.java
@@ -13,8 +13,11 @@ public class Sheet {
     }
 
     public XSSFCell getRange(String rangeStr) {
-        if (rangeStr.length() == 2 && rangeStr.matches("[a-zA-Z][0-9]+")) {
-            return getCell(Integer.valueOf(rangeStr.replaceAll("[^0-9]+", "")) - 1, JExcel.Cell(rangeStr.replaceAll("[0-9]+", "")));
+        if (rangeStr.matches("[a-zA-Z][0-9]+")) {
+            Integer row = Integer.valueOf(rangeStr.replaceAll("[^0-9]+", "")) - 1;
+            Integer cell = JExcel.Cell(rangeStr.replaceAll("[0-9]+", ""));
+
+            return getCell(row, cell);
         } else {
             return getCell(0, 0);
         }
